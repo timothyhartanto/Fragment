ed9d668ee8f7a5812ff67d3f62f3b20930f65a0c
timothyhartanto <hartantotimothy@gmail.com>
2016-01-23 23:08:24 +0700

    Update and rename ListFragment.java to MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.proto.fragmentexample;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.proto.simplefrag.SimpleFragmentActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void simple(View v){
+        Intent intent = new Intent(getApplicationContext(), SimpleFragmentActivity.class);
+        startActivity(intent);
+    }
+
+ 
+}
